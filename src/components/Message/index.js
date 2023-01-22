d4c2c57f3c241ce25988e4119b82986af4a64f5c
Aman <amansaxena2809@gmail.com>
2023-01-22 20:39:25 +0530

    Chat Screen Completed

---
+++ src/components/Message/index.js
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet } from "react-native";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+const Message = ({ message }) => {
+  const isMymessage=()=>{
+    return message.user.id==='u1';
+  }
+  return (
+    <View style={[
+      styles.container,
+      {
+        backgroundColor:isMymessage()?'#DCF8C5':'white',
+        alignSelf:isMymessage()?'flex-end':'flex-start'
+      }]}>
+      <Text>{message.text}</Text>
+      <Text style={styles.time}>{dayjs(message.createdAt).fromNow(true)}</Text>
+    </View>
+  );
+};
+
+const styles=StyleSheet.create({
+  container:{
+    backgroundColor:'white',
+    margin:5,
+    padding:10,
+    borderRadius:10,
+    maxWidth:'80%'
+  },
+  time:{
+    color:'grey',
+    alignSelf:'flex-end'
+  }
+});
+export default Message;
